88589ef23393d4f742014ce0eb8eb4829d449480
Ieva Sinkeviciute <ieva1919@gmail.com>
2020-10-17 20:29:19 +0300

    add icons

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,9 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
 import { Nav, Navbar } from 'react-bootstrap';
 import {
     Link
 } from "react-router-dom";
 import "./Menu.scss"
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+
 
 
 function Menu() {
@@ -12,11 +15,11 @@ function Menu() {
             <div className="container">
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Link to="/">
+                    <Link to="/home">
                         <Navbar.Brand> Ristorante Con Furion</Navbar.Brand>
                     </Link>
                     <Nav >
-                        <Nav.Link className="nav-item "><Link className="nav-link" to="/home">Home</Link></Nav.Link>
+                        <Nav.Link className="nav-item "><FontAwesomeIcon icon={faHome} /><Link className="nav-link" to="/home">Home</Link></Nav.Link>
                         <Nav.Link className="nav-item"><Link className="nav-link" to="/about">About</Link></Nav.Link>
                         <Nav.Link className="nav-item"><Link className="nav-link" to="/menu">Menu</Link></Nav.Link>
                         <Nav.Link className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></Nav.Link>
