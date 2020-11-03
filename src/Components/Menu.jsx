bc575e179945c6eb1d17ad6adc41fa090cbef8b4
Ieva Sinkeviciute <ieva1919@gmail.com>
2020-11-03 15:49:33 +0200

    add more styles in the menu and header

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -11,7 +11,6 @@ import { faList } from '@fortawesome/free-solid-svg-icons'
 import { faAddressCard } from '@fortawesome/free-solid-svg-icons'
 
 
-
 function Menu() {
     const [show, setShow] = useState(false);
     return (
@@ -27,43 +26,42 @@ function Menu() {
                         <Nav.Link as={Link} to="/about" className="nav-item"><FontAwesomeIcon icon={faInfo} className="mr-2" />About</Nav.Link>
                         <Nav.Link as={Link} to="/menu" className="nav-item"><FontAwesomeIcon icon={faList} className="mr-2" />Menu</Nav.Link>
                         <Nav.Link as={Link} to="/contact" className="nav-item"><FontAwesomeIcon icon={faAddressCard} className="mr-2" />Contact</Nav.Link>
+                    </Nav >
+                    <div className="d-flex loginmodal" style={{ flexGrow: 1, justifyContent: 'flex-end' }}>
                         <Button onClick={() => setShow(true)}>
                             <FontAwesomeIcon icon={faSignInAlt} className="mr-2" /> Login
                         </Button>
-                        <Modal
-                            show={show}
-                            onHide={() => setShow(false)}
-                            dialogClassName="modal-90w"
-                            aria-labelledby="example-custom-modal-styling-title"
-                        >
-                            <Modal.Header closeButton>
-                                <Modal.Title id="example-custom-modal-styling-title">
-                                    Login
+                    </div>
+                    <Modal
+                        className="login"
+                        show={show}
+                        onHide={() => setShow(false)}
+                        dialogClassName="modal-90w"
+                        aria-labelledby="example-custom-modal-styling-title"
+                    >
+                        <Modal.Header closeButton className="reserveatble">
+                            <Modal.Title id="example-custom-modal-styling-title">
+                                Login
                         </Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>
-                                <Form>
-                                    <Form.Group controlId="formBasicCheckbox">
-                                        <Form.Control type="email" placeholder="Enter email" />
-                                    </Form.Group>
-                                    <Form.Group controlId="formBasicCheckbox">
-                                        <Form.Control type="password" placeholder="Password" />
-                                    </Form.Group>
-                                    <Form.Group controlId="formBasicCheckbox">
-                                        <Form.Check type="checkbox" label="Remember me" />
-                                    </Form.Group>
-                                    <Form.Group>
-                                        <Button variant="secondary" type="submit" className="btn-sm ml-auto" onClick={() => setShow(false)}>
-                                            Cancel
+                        </Modal.Header>
+                        <Modal.Body className="modalbody">
+                            <Form>
+                                <Form.Group controlId="formBasicCheckbox">
+                                    <Form.Control type="email" placeholder="Enter email" />
+                                    <Form.Control type="password" placeholder="Password" />
+                                    <Form.Check type="checkbox" label="Remember me" />
+                                </Form.Group>
+                                <Form.Group>
+                                    <Button variant="secondary" type="submit" className="btn-sm ml-auto" onClick={() => setShow(false)}>
+                                        Cancel
                                         </Button>
-                                        <Button variant="primary" type="submit" className="btn-sm ml-1" onClick={() => setShow(false)}>
-                                            Sign in
+                                    <Button variant="primary" type="submit" className="btn-sm ml-1" onClick={() => setShow(false)}>
+                                        Sign in
                                         </Button>
-                                    </Form.Group>
-                                </Form>
-                            </Modal.Body>
-                        </Modal>
-                    </Nav >
+                                </Form.Group>
+                            </Form>
+                        </Modal.Body>
+                    </Modal>
                 </Navbar.Collapse>
             </div>
         </Navbar >
